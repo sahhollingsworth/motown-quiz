97fd1e1340d0a5e0065f21d0c45d4414e67ff54f
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-09-28 01:32:30 -0700

    Feat: Added Leaderboard logic for redering any scores in local storage. Added quiz end UI to complete both happy path (answered all questions) and unhappy path (ran out of time).

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,18 +46,21 @@ var timeRemain;
 /* indicates 1st object in the questions array is the first question shown to user */
 var questionIndex = 0;
 
-/* Add listener for start button */
-startBtn.addEventListener("click", quizStart);
+/* Listener for quiz START button */
+startBtn.onclick = quizStart;
 
 function quizStart() {
-    /* hides the welcome UI */
+    /* hide the welcome UI */
     welcomeEL.setAttribute("class","hidden");
-    /* unhides the question UI */
-    questionEl.setAttribute("class","show");
+    
+    /* unhide the question UI */
+    questionEl.setAttribute("class","");
+    
     /* Starts timer. Calls the time remaining function every 1 second */
     timeRemain = setInterval(function(){
         timeRemaining(); 
     }, 1000);
+    
     /* Display initial time */
     timerEL.textContent = time;
     renderQuestion();
@@ -68,6 +71,7 @@ function timeRemaining(){
     /* continuous subtraction from time and update UI */
     time--;
     timerEL.textContent = time;
+    
     /* check that there is still time, otherwise, end quiz. This works but the UI is FUBAR */
     // if (time > 0 && time <= 20){
     //     var timer = document.getElementById("timer");
@@ -75,7 +79,7 @@ function timeRemaining(){
     //     timerEL.setAttribute("class","time-low");
     // }
     if (time <= 0){
-        /* quizEnd(); later that defines what happens when quiz ends*/
+        quizEnd();
     }
 }
 
@@ -83,8 +87,10 @@ function timeRemaining(){
 function renderQuestion(){
     /* get a question from the questionSet array */
     var question = questionSet[questionIndex];
-    // update title with active question object
+    
+    /* update title with active question object */
     questionEl.children[0].textContent = question.title;
+    
     /* evaluates if sometimes */
     while (answersEl.hasChildNodes()) {
         answersEl.removeChild(answersEl.lastChild);
@@ -106,7 +112,6 @@ function renderQuestion(){
     });
     answersEl.children[2].addEventListener("click", function(event){
         optionClick(answersEl.children[2]);
-        console.log("you hit an answer good job");
     });
     answersEl.children[3].addEventListener("click", function(event){
         optionClick(answersEl.children[3]);
@@ -116,10 +121,12 @@ function renderQuestion(){
         /* if the answer option the user clicks on matches in type and value to the answer in the questionSet array, flash the user a Correct toast */
         if(option.textContent == questionSet[questionIndex].answer){
             toastEl.textContent = "Correct!";
+        
         /* if the answer option the user clicks on doesn't match in type and value to the answer in the questionSet array, flash the user a Wrong toast and  */
         } else {
+            
             /* subraction assingment to remove 2 seconds from time remaining if the answer is wrong */
-            time -= 5;
+            time -= 10;
             toastEl.textContent = "Wrong :/";
         }
 
@@ -128,14 +135,82 @@ function renderQuestion(){
 
         /* Evaluate if all questions in the array have been used yet */
         if (questionIndex === questionSet.length) {
-            console.log("no more");
-            /* quizEnd(); later that defines what happens when quiz ends*/
+            quizEnd();
         } else {
-            console.log("render another question");
             renderQuestion();
         }
     }
 }
-/* Leaderboard UI */
-/* Listen for click on the leaderboard-ref link to unhide Leaderboard section */
-/* leaderboardBtn.addEventListener("click", ); */
+
+/* Render end page with score and initials entry form */
+function quizEnd(){
+    /* stop timer */
+    clearInterval(timeRemain);
+    timerEL.textContent = time;
+    
+    /* Return quiz score */
+    var score = document.getElementById("score");
+    score.textContent = time;
+    
+    /* hide the question UI */
+    questionEl.setAttribute("class","hidden");
+    
+    /* unhide the end UI */
+    var endEL = document.getElementById("end");
+    endEL.setAttribute("class", "");
+}
+
+/* Listener for initials SUBMIT button */
+submitBtn.onclick = saveScore;
+
+/* Listener for key release - to evaluate if enter was used instead of clicking initials SUBMIT button */
+initialsEl.onkeyup = checkKey;
+
+/* Evaluate if the key pressed was the enter key for form submission */
+function checkKey(event) {
+    /* if the released key was the enter key, then act as form submission of initials value */
+    if (event.keyCode === 13){
+        saveScore();
+    }
+}
+
+
+function saveScore(){
+    /* standardize value provided in initials field */
+    var initials = initialsEl.value.toUpperCase();
+    console.log(initials);
+    initials.trim();
+    console.log(initials);
+    
+    /* validation to prevent blank form entry */
+    if (initials === ""){
+        alert("Please provide initials");
+        saveScore;
+    } else {
+        var highscores;
+
+        /* Get any highscores present in local storage */
+        if (JSON.parse(localStorage.getItem("highscores")) != null) {
+            highscores = JSON.parse(window.localStorage.getItem("highscores"));
+        
+        /* if no high scores in local storage, create an empty array */
+        } else {
+            highscores = []
+        }
+        /* create an object for current completion */
+        var newScore = {
+            initials: initials,
+            score: time,
+        };
+
+        /* Add object created for current completion to the end of the highscores array */
+        highscores.push(newScore);
+        
+        /* Save Hish Scores array to local storage as string */
+        localStorage.setItem("highscores",JSON.stringify(highscores));
+
+        location.href = "leaderboards.html";
+    }
+}
+
+// leaderboardBtn.addEventListener("click", );
